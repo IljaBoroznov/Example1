b6a534d9926ad5ee39bf104e31d81d4316297c6a
Илья <ilja_boroznov@mail.ru>
2022-09-16 12:52:12 +0300

    ДЗ-1

---
+++ ДЗ-1/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 5;
+int b = 7;
+int max = a;
+int min = b;
+
+if(a > b) 
+{
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min); 
+}
+
+if(a < b) 
+{
+max = b;
+min = a;
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min); 
+}
+
