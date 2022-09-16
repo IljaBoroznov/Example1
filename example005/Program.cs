cf242776a854d35df81a5a316a17a3802bc71f8b
Илья <ilja_boroznov@mail.ru>
2022-09-16 11:10:19 +0300

    Добавили приветствие разных  пользователей

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "илья")
+{
+    Console.WriteLine("Ура");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
