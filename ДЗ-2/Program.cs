529dbd61aa24ae37d10dd03ee31d3087f9c507db
Илья <ilja_boroznov@mail.ru>
2022-09-16 13:25:55 +0300

    ДЗ-2

---
+++ ДЗ-2/Program.cs
@@ -0,0 +1,23 @@
+﻿int first = 2;
+int second = 3;
+int third = 7;
+int max = 0;
+
+if (second < first)
+{
+    max = first;
+}
+else
+{
+    max = second;
+}
+if (third < max)
+{
+   Console.Write("max = ");
+   Console.WriteLine(max); 
+}
+else
+{
+   Console.Write("max = ");
+   Console.WriteLine(third);  
+}
